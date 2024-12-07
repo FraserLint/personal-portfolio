1594b4cbe8f067a13ee8e9ac33155c3b9b808f21
Austin Davison <davisonaustin287@gmail.com>
2024-12-07 20:52:18 +0000

    Updated to use imgur for image hosting to work with github actions deployment

--- src/components/data/projects-list.tsx
+++ src/components/data/projects-list.tsx
@@ -4,14 +4,14 @@ export const projects = [
         title: "Dock Street Studios Photography Website",
         description: "An innovative redesign for Dock Street Studios SEO optimised website when moving to a new location and rebranding. " +
             "Using web design tools such as Webflow to create an easy to manage CMS.",
-        image: "/project_screenshots/dock_street_website.png",
+        image: "https://i.imgur.com/BHimpND.png",
         link: "https://www.dockstreetstudioleeds.co.uk/"
     },
     {
         id: 2,
         title: "Swift TODO App",
         description: "A little to do list app written in swift to teach me Swift, SwiftUI, SwiftData and apple development.",
-        image: "/project_screenshots/swift_to_do.png",
+        image: "https://i.imgur.com/UO7pPDB.png",
         link: "https://github.com/FraserLint/swift-to-do"
     },
     {
@@ -19,7 +19,7 @@ export const projects = [
         title: "Angular Employee Manager",
         description: "A simple employee manager written in Angular with the help of Angular Material to teach me the angular framework," +
             " and use the material design system. While simultaneously re-enforcing my knowledge of CRUD applications.",
-        image: "/project_screenshots/employee_manager.png",
+        image: "https://i.imgur.com/TYIBP11.png",
         link: "https://github.com/FraserLint/employee-manager"
     },
     {
@@ -27,7 +27,7 @@ export const projects = [
         title: "Personal Portfolio Site ( this one 😂)",
         description: "A place to showcase a bit about me, my skills, projects & qualifications. " +
             "Written using NextJS (TypeScript) with ShadCN and TailwindCSS.",
-        image: "/project_screenshots/personal_portfolio.png",
+        image: "https://i.imgur.com/Ss8l8sV.png",
         link: "https://github.com/FraserLint/personal-portfolio"
     },
     {
@@ -35,7 +35,7 @@ export const projects = [
         title: "Happy2Help: A Discord Chatbot",
         description: "A Discord chatbot to do all the things a good discord bot should do using the python pycord lib, " +
             "music, kick, ban, mute/unmute, clear, 8ball. (I wrote this at age 14 and it was got me properly hooked with programming. ",
-        image: "/project_screenshots/happy_2_help.png",
+        image: "https://i.imgur.com/Y9B5csv.png",
         link: "https://github.com/FraserLint/Happy2Help"
     },
     {
@@ -43,7 +43,7 @@ export const projects = [
         title: "React Native TODO App",
         description: "A Small React Native + Expo TODO application to teach myself the fundamentals of the framework, " +
             "as well as getting used the mobile app development experience using web technologies. ",
-        image: "/project_screenshots/todo_react_native.png",
+        image: "https://i.imgur.com/49bE5RL.png",
         link: "https://github.com/FraserLint/todo-react-native"
     }
 ]
