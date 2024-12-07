127b4485290b30dcfb7782b8657e4ca88b8c9435
Austin Davison <davisonaustin287@gmail.com>
2024-12-07 21:00:59 +0000

    Fixed missing header title on mobile

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 import { useTheme } from "next-themes"
 import { Moon, Sun } from 'lucide-react'
 import { Button } from "@/components/ui/button"
-import Link from "next/link";
+import Link from "next/link"
 
 export function Header() {
     const { theme, setTheme } = useTheme()
@@ -11,17 +11,16 @@ export function Header() {
     return (
         <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/80 backdrop-blur-xl backdrop-saturate-150 supports-[backdrop-filter]:bg-background/60">
             <div className="container flex h-14 items-center justify-between px-4 md:px-6 max-w-screen-2xl mx-auto">
-                <div className="mr-4 hidden md:flex">
-                    <Link className="mr-6 flex items-center space-x-2" href="/"><span
-                        className="hidden font-bold sm:inline-block">Austin Davison</span>
+                <div className="flex items-center">
+                    <Link className="flex items-center space-x-2" href="/">
+                        <span className="font-bold">Austin Davison</span>
                     </Link>
                 </div>
-                <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+                <div className="flex items-center">
                     <Button
                         variant="ghost"
                         size="icon"
                         aria-label="Toggle theme"
-                        className=""
                         onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
                     >
                         <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
