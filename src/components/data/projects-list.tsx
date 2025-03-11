3e44efb6700f616a9b7684cffd7f1579b01f9668
Austin Davison <davisonaustin287@gmail.com>
2025-03-11 17:49:01 +0000

    Updated projects list

--- src/components/data/projects-list.tsx
+++ src/components/data/projects-list.tsx
@@ -1,6 +1,13 @@
 export const projects = [
     {
         id: 1,
+        title: "RZA Zoo Mock Website",
+        description: "A mock project (in the form of a fictional website for RZA Zoo) for my 2nd year T-Level in Digital Production Design & Development course occupational specialism task.",
+        image: "https://i.imgur.com/QdX3474.png",
+        link: "https://github.com/FraserLint/rza-zoo-tlevel-dpdd-os-mock"
+    },
+    {
+        id: 2,
         title: "Dock Street Studios Photography Website",
         description: "An innovative redesign for Dock Street Studios SEO optimised website when moving to a new location and rebranding. " +
             "Using web design tools such as Webflow to create an easy to manage CMS.",
@@ -8,14 +15,14 @@ export const projects = [
         link: "https://www.dockstreetstudioleeds.co.uk/"
     },
     {
-        id: 2,
+        id: 3,
         title: "Swift TODO App",
         description: "A little to do list app written in swift to teach me Swift, SwiftUI, SwiftData and apple development.",
         image: "https://i.imgur.com/UO7pPDB.png",
         link: "https://github.com/FraserLint/swift-to-do"
     },
     {
-        id: 3,
+        id: 4,
         title: "Angular Employee Manager",
         description: "A simple employee manager written in Angular with the help of Angular Material to teach me the angular framework," +
             " and use the material design system. While simultaneously re-enforcing my knowledge of CRUD applications.",
@@ -23,15 +30,15 @@ export const projects = [
         link: "https://github.com/FraserLint/employee-manager"
     },
     {
-        id: 4,
+        id: 5,
         title: "Personal Portfolio Site ( this one 😂)",
         description: "A place to showcase a bit about me, my skills, projects & qualifications. " +
             "Written using NextJS (TypeScript) with ShadCN and TailwindCSS.",
-        image: "https://i.imgur.com/Ss8l8sV.png",
+        image: "https://i.imgur.com/7zoyIxy.png",
         link: "https://github.com/FraserLint/personal-portfolio"
     },
     {
-        id: 5,
+        id: 6,
         title: "Happy2Help: A Discord Chatbot",
         description: "A Discord chatbot to do all the things a good discord bot should do (music, kick, ban, mute/unmute, clear, 8ball) " +
             "- using the python pycord library. " +
@@ -40,7 +47,7 @@ export const projects = [
         link: "https://github.com/FraserLint/Happy2Help"
     },
     {
-        id: 6,
+        id: 7,
         title: "React Native TODO App",
         description: "A Small React Native + Expo TODO application to teach myself the fundamentals of the framework, " +
             "as well as getting used the mobile app development experience using web technologies. ",
